17bc8bd7f10917d4544b9e326f44574ace8bac90
Axionexa Solutions <axionexasolutions@gmail.com>
2026-08-01 18:47:27 +0600

    updated the metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Professional Cleaning Services in Willenhall, Wolverhampton & West Midlands | Refuse Shine Cleaning LTD",
-  description: "ETrusted professional cleaning services across Willenhall, Wolverhampton, Walsall, Dudley, Birmingham and the West Midlands. Book regular house cleaning, deep cleaning, end of tenancy cleaning, carpet cleaning and more.",
+  description: "Trusted professional cleaning services across Willenhall, Wolverhampton, Walsall, Dudley, Birmingham and the West Midlands. Book regular house cleaning, deep cleaning, end of tenancy cleaning, carpet cleaning and more.",
   alternates: {
     canonical: "/",
   },
