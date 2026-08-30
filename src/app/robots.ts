efff6a7b8629593222e00fec634d77db5554c912
Axionexa Solutions <axionexasolutions@gmail.com>
2026-08-30 15:51:15 +0600

    robots.txt is changes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,10 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/"
+      allow: "/",
+      disallow: [
+        "/book?*"
+      ]
     },
     sitemap: "https://www.refuseshinecleaningltd.co.uk/sitemap.xml",
   };
