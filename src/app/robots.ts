db5622a871c2d6f8a94b976db5b3be260a144eab
Axionexa Solutions <axionexasolutions@gmail.com>
2026-08-30 15:42:18 +0600

    update the sitemap and internal linking

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,10 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/*?*"
-      ]
+      allow: "/"
     },
     sitemap: "https://www.refuseshinecleaningltd.co.uk/sitemap.xml",
   };
